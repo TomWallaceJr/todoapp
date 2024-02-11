9907b8cb8cc9961fdae82274bd21fc0e70e9cb7e
Tom <twallace1015@gmail.com>
2024-02-11 15:48:37 -0500

    hh

--- app/components/AddTask.tsx
+++ app/components/AddTask.tsx
@@ -3,7 +3,7 @@ import { FaPlus } from "react-icons/fa";
 
 const AddTask = () => {
   return (
-    <div>
+    <div className='bg-red-500'>
       <button className='btn btn-primary w-full'>
         Add New Task
         <FaPlus className='ml-2' size={18} />
